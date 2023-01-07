abe912b52eade89527f6cf16746b6404b2d78921
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2023-01-07 10:38:36 +0100

    added className tags in Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css'
 
 const Navbar = () => {
    return ( 
-      <div>
-         Navbar
-      </div>
+      <nav className={styles.navbar}>
+         <ul>
+            <li className={styles.title}>Hajsownik</li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+         </ul>
+      </nav>
     );
 }
  
