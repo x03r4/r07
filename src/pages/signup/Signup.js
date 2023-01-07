51632c967b0b5bcc3aca5262e73c5dadb86a37e5
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2023-01-07 11:53:03 +0100

    created Signup form with submit functionality to the Signup.js file

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,10 +1,49 @@
+import { useState } from 'react';
 import styles from './Signup.module.css'
 
 const Signup = () => {
+
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => { 
+    e.preventDefault()
+    console.log(userName, email, password) 
+  }
+
    return ( 
-      <div>
-         Signup
-      </div>
+      <form 
+        onSubmit={handleSubmit}
+        className={styles['signup-form']}
+      >
+        <h2>Sign Up</h2>
+        <label>
+          <span>User Name:</span>
+          <input 
+            type="text"
+            onChange={(e)=>setUserName(e.target.value)}
+            value={userName} 
+          />
+        </label>
+        <label>
+          <span>email:</span>
+          <input 
+            type="email"
+            onChange={(e)=>setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label>
+          <span>password:</span>
+          <input 
+            type="password"
+            onChange={(e)=>setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        <button className="btn">Sign Up</button>
+      </form>
     );
 }
  
